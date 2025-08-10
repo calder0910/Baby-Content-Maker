c70ebe9d2dae7554673233ab51372c4035609afa
Rachmat Wahyudi <rachmatsyalaffiliate@gmail.com>
2025-08-11 00:52:55 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Baby-Content-Maker/', // penting untuk GitHub Pages
-  build: {
-    outDir: 'dist', // output default untuk Vite
-    emptyOutDir: true // pastikan folder dist dibersihkan sebelum build
-  },
-  server: {
-    port: 3000,
-    open: true
-  }
+  base: '/Baby-Content-Maker/',
 })
