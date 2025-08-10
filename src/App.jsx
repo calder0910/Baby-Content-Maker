eca7b65ccbe83426df5767f9ee1e1dde79f52eb9
Rachmat Wahyudi <rachmatsyalaffiliate@gmail.com>
2025-08-11 00:34:55 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import BabyContentGenerator from './components/BabyContentGenerator'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <BabyContentGenerator />
+    </div>
+  )
+}
+
+export default App
